b5eb50afc5de20de6dbea12e3d73353be6cd584e
pwolle <ihaveunitdeterminant@gmail.com>
2025-03-09 23:20:42 +0100

    add initializers

---
+++ src/seli/net/_initializers.py
@@ -0,0 +1,109 @@
+from collections.abc import Sequence
+from typing import Any, Literal
+
+import jax
+import jax.numpy as jnp
+import jax.random as jrn
+from jax.typing import DTypeLike
+from jaxtyping import PRNGKeyArray
+
+from seli import Module, typecheck
+
+
+class Initializer(Module, "initializers.Initializer"):
+    def __call__(
+        self,
+        key: PRNGKeyArray,
+        shape: Sequence[int],
+        dtype: DTypeLike = jnp.float32,
+        **_: Any,
+    ) -> jax.Array:
+        raise NotImplementedError("Initializers must be implemented")
+
+
+class Zeros(Initializer):
+    def __call__(
+        self,
+        key: PRNGKeyArray,
+        shape: Sequence[int],
+        dtype: DTypeLike = jnp.float32,
+        scale: float = 1.0,
+    ) -> jax.Array:
+        return jnp.zeros(shape, dtype)
+
+
+class Ones(Initializer):
+    def __call__(
+        self,
+        key: PRNGKeyArray,
+        shape: Sequence[int],
+        dtype: DTypeLike = jnp.float32,
+    ) -> jax.Array:
+        return jnp.ones(shape, dtype)
+
+
+@typecheck
+class Normal(Initializer):
+    """
+    Initialize weights from a normal distribution. Weights can be scaled by
+    the number of input or output units, or by the sum of input and output,
+    or by a custom factor. The methods provided are based on
+    - He et al. (2015): https://arxiv.org/abs/1502.01852
+    - Glorot & Bengio (2010): https://proceedings.mlr.press/v9/glorot10a.html
+    """
+
+    def __call__(
+        self,
+        key: PRNGKeyArray,
+        shape: Sequence[int],
+        dtype: DTypeLike = jnp.float32,
+        scale: float | Literal["He", "Glorot"] | None = None,
+    ) -> jax.Array:
+        if len(shape) == 2 and scale is None:
+            scale = "He"
+
+        w = jrn.normal(key, shape, dtype)
+
+        if scale == "He":
+            assert len(shape) == 2
+            return w * jnp.sqrt(2.0 / shape[0])
+
+        if scale == "Glorot" or scale == "Xavier":
+            assert len(shape) == 2
+            return w * jnp.sqrt(2.0 / (shape[0] + shape[1]))
+
+        assert isinstance(scale, float)
+        return w * scale
+
+
+class Uniform(Initializer):
+    """
+    Initialize weights from a uniform distribution. Weights can be scaled by
+    the number of input or output units, or by the sum of input and output,
+    or by a custom factor. The methods provided are based on
+    - He et al. (2015): https://arxiv.org/abs/1502.01852
+    - Glorot & Bengio (2010): https://proceedings.mlr.press/v9/glorot10a.html
+    """
+
+    def __call__(
+        self,
+        key: PRNGKeyArray,
+        shape: Sequence[int],
+        dtype: DTypeLike = jnp.float32,
+        scale: float | Literal["He", "Glorot", "Xavier"] | None = None,
+    ) -> jax.Array:
+        if len(shape) == 2 and scale is None:
+            scale = "He"
+
+        w = jrn.uniform(key, shape, dtype, minval=-1, maxval=1)
+
+        if scale == "He":
+            assert len(shape) == 2
+            return w * jnp.sqrt(2.0 / shape[0])
+
+        if scale == "Glorot" or scale == "Xavier":
+            assert len(shape) == 2
+            return w * jnp.sqrt(2.0 / (shape[0] + shape[1]))
+
+        assert isinstance(scale, float)
+        return w * scale
--- src/seli/net/_param.py
+++ src/seli/net/_param.py
@@ -1,14 +1,34 @@
-import jax
+# @typecheck
+# class Param(Module):
+#     def __init__(
+#         self,
+#         value: jax.Array,
+#         /,
+#         trainable: bool = True,
+#         collections: list[str] | None = None,
+#     ):
+#         self.value = value
+#         self.trainable = trainable
+#         self._collections = collections or []
 
-from seli import Module
+#     def __call__(self) -> jax.Array:
+#         return self.value
+
+#     def __repr__(self) -> str:
+#         return f"Param({self.value})"
+
+#     @property
+#     def collections(self) -> list[str]:
+#         collection = []
+
+#         if self.trainable:
+#             collection.append("trainable")
+
+#         collection.extend(self._collections)
+#         return collection
 
 
-class Param(Module):
-    def __init__(self, value: jax.Array):
-        self.value = value
-
-    def __call__(self) -> jax.Array:
-        return self.value
-
-    def __repr__(self):
-        return f"Param({self.value})"
+# @typecheck
+# class Param(Module):
+#     def __init__(self, inti):
+#         pass
